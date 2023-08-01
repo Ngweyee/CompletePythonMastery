2a6937bd14ebd40e2b858530702fca263b69c2ec
Saruchi Hunjan <shunjan@adherahealth.com>
2023-08-01 15:13:29 +0800

    - Primitive Types

--- main.py
+++ main.py
@@ -1,16 +1,83 @@
-# This is a sample Python script.
+# Variables && Variable Names
+import math
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+student_count = 1000
+rating = 4.99
+is_published = False
+course_name = "Python Programming"
+print(student_count)
+print(rating)
+print(is_published)
+print(course_name)
+
+# Strings
+course = "Python Programming"
+print(len(course))
+print(course[0])
+print(course[-1])
+print(course[0:3])
+print(course[0:])
+print(course[:3])
+print(course[:])
+
+# Escape Sequences
+# \'
+# \"
+# \\
+# \n
+
+course = "Eli's Python 'Programming Journey"
+print(course)
+
+# Formatted String
+first = "Ngwe Yee"
+second = "Shoon"
+full = first + " " + second
+print(full)
+full_name = f"{first} {second}"
+full_name = f"{len(first)} {2 + 2}"
+print(full_name)
+
+# String Methods
+course = " python programming "
+print(course.upper())
+print(course.lower())
+print(course.title())
+print(course.strip())
+print(course.lstrip())
+print(course.rstrip())
+print(course.find("pro"))  # return index
+print(course.replace("p", "j"))
+print("pro" in course)
+print("swift" not in course)
+
+# Numbers
+print(10 + 3)
+print(10 - 3)
+print(10 * 3)
+print(10 / 3)
+print(10 % 3)
+print(10 // 3)
+print(10**3)
+
+x = 10
+x = x + 3
+x += 3
+print(x)
+
+# Working with Numbers
+print(round(2.9))
+print(abs(2.2))
+print(math.ceil(2.3))
+print(math.floor(2.5))
+print(math.factorial(5))
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# Type Conversion
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(bool(0))
+print(bool(1))
+print(bool(-1))
+print(bool(5))
+print(bool(""))
+print(bool("False"))
