c17d4f539d6ebd6fde713ff57659a5c3e72aaa11
Saruchi Hunjan <shunjan@adherahealth.com>
2023-08-12 12:36:59 +0800

    - Exceptions

--- main.py
+++ main.py
@@ -1,16 +1,83 @@
-# This is a sample Python script.
+from timeit import timeit  # to calculate the execution time of code
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# 1- Exceptions
+# date = int(input("Enter Date: ")) # Error
+# print(date)
+
+# 2- Handling Exception & 3- Handling Different Exceptions
+try:
+    age = int(input("Enter Age 1: "))
+    xFactor = 10 / age
+except (ValueError, ZeroDivisionError):  # handling more than one exception
+    print("You didn't enter a valid age")
+else:
+    print("No exceptions were thrown.")
+    print("Execution continues.")
+
+# 4- Cleaning Up
+try:
+    myfile = open("app.py")
+    age = int(input("Enter Age 2 : "))
+    xFactor = 10 / age
+except (ValueError, ZeroDivisionError):  # handling more than one exception
+    print("You didn't enter a valid age")
+except FileNotFoundError:
+    print("There is no such file")
+else:
+    print("No exceptions were thrown.")
+finally:
+    myfile.close()
+
+# 5- The With Statement
+# 4- Cleaning Up
+try:
+    with open("app.py") as file:
+        print("File is opened.")
+    age = int(input("Enter Age 3 : "))
+    xFactor = 10 / age
+except (ValueError, ZeroDivisionError):  # handling more than one exception
+    print("You didn't enter a valid age")
+else:
+    print("No exceptions were thrown.")
+
+# 6- Raising Exceptions
+def calculate_xfactor(age):
+    if age <= 0:
+        raise ValueError("Age cannot be 0 or less.")
+    return 10 / age
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+try:
+    calculate_xfactor(-1)
+except ValueError as error:
+    print(error)
+
+# 7- Cost of Exceptions
+first_code = """ 
+def calculate_xfactor(age):
+    if age <= 0:
+        raise ValueError("Age cannot be 0 or less.")
+    return 10 / age
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+try:
+    calculate_xfactor(-1)
+except ValueError as error:
+    print(error)
+"""
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+second_code = """ 
+def calculate_xfactor(age):
+    if age <= 0:
+        return None
+    return 10 / age
+
+
+
+xfactor = calculate_xfactor(-1)
+if xfactor == None:
+    pass
+"""
+
+print("Exectuion time for First Code :", timeit(first_code, number=10000))
+print("Exectuion time for Second Code :", timeit(second_code, number=10000))
